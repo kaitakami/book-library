3ade5e0ea9fb2acb1266c44c076377da8628af44
Kai Takami <kaitakami@protonmail.com>
2022-06-07 19:58:26 +0900

    Finished version with function constructor

--- app.js
+++ app.js
@@ -1,71 +1,86 @@
 let myLibrary = [];
 
-class Book {
-    constructor(bookName, author, pages, image, color) {
-        this.bookName = bookName;
-        this.author = author;
-        this.pages = pages;
-        this.image = image;
-        this.color = color;
-    }
+// Function constructor
+function Book(title, author, pages, color) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.color = color;
 }
 
-function addBookToLibrary () {
-    const book = Object.create(Book);
-    book.bookName = nameInput.value;
-    book.author = authorInput.value;
-    book.pages = pagesInput.value;
-    book.image = imageInput.value;
-    book.color = colorInput.value;
+// The magic
+function addBookToLibrary() {
+    const title = nameInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const color = colorInput.value;
 
-    // Clear all cards and re-display cards again
-    let child = mainContainer.lastElementChild;
+    // Create new object
+    const book = new Book(title, author, pages, color)
 
-    while (child) {
-        mainContainer.removeChild(child);
-        child = mainContainer.lastElementChild;
-    }
+    myLibrary.push(book);
 
-    for (eachBook in myLibrary) {
-        const card = mainContainer.appendChild(document.createElement('div'));
-        card.classList.add('card');
-        // Add image to card div
-        const bookImage = card.appendChild(document.createElement('img'));
-        this.bookImage.classList.add('book__image');
-        this.bookImage.src = book.image;
-        console.log(Boolean(book.image))
-        if (!book.image) {
-            bookImage.src = 'https://dl.airtable.com/.attachments/b542a976abd2eff3694039cc7bdbe7c1/51b858b7/unicornfaunda.png?ts=1654398641&userId=usrUSBuAFGnnFGzvL&cs=54eee2291345f081';
+    //Validate
+    if (title === '' || author === '' || pages === '') {
+        alert('Fill all the blanks');
+    } else {
+        // Display book DOM
+        const cardDisplay = mainContainer.appendChild(document.createElement('div'));
+        cardDisplay.classList.add('card');
+        const colorDisplay = cardDisplay.appendChild(document.createElement('span'));
+        colorDisplay.classList.add('card__color');
+        colorDisplay.style.backgroundColor = color;
+        const cardContent = cardDisplay.appendChild(document.createElement('div'));
+        cardContent.classList.add('card__content');
+        const titleDisplay = cardContent.appendChild(document.createElement('h3'));
+        titleDisplay.classList.add('book-title', 'card__text');
+        titleDisplay.textContent = `Title: ${title}`;
+        const authorDisplay = cardContent.appendChild(document.createElement('p'));
+        authorDisplay.classList.add('book-author', 'card__text');
+        authorDisplay.textContent = `Author: ${author}`;
+        const pagesDisplay = cardContent.appendChild(document.createElement('p'));
+        pagesDisplay.classList.add('book-pages', 'card__text');
+        pagesDisplay.textContent = `Pages: ${pages}`;
+
+        // Remove button DOM
+        const removeButtonDisplay = cardContent.appendChild(document.createElement('i'));
+        removeButtonDisplay.classList.add('fa-solid', 'fa-circle-minus', 'remove-button');
+
+        // Remove button
+        card = document.querySelectorAll('.card');
+        for (let i = 0; i < card.length; i++) {
+            card[i].addEventListener('click', (e) => {
+                if (e.target.classList.contains('remove-button')) {
+                    e.target.parentElement.parentElement.remove()
+                }
+            })
         }
-        // Add color bar
-        const barColor = card.appendChild(document.createElement('span'));
-        barColor.classList.add('card__color');
-        barColor.style.backgroundColor = '#' + book.color;
-        // Create card content
-        const cardContainer = card.appendChild(document.createElement('div'));
-        cardContainer.classList.add('card__container');
 
-        // Add book title to card div
-        // const bookTitle = cardContent.appendChild(h3);
-        // bookTitle.classList.add('book-title', 'card__text');
-        // bookTitle.textContent = book.bookName;
+        // Clear form
+        nameInput.value = '';
+        authorInput.value = '';
+        pagesInput.value = '';
+        colorInput.value = '#000000';
+
+        // Close form
+        cardInputDisplay.classList.remove('active');
     }
-    myLibrary.push(book)
 }
+
 
 
 // DOM
 const addButton = document.querySelector('.add-button');
 const closeButton = document.querySelector('.close-button');
 const submitButton = document.querySelector('.submit-button');
+const removeButton = document.querySelectorAll('.remove-button');
 const cardInputDisplay = document.querySelector('.input-card');
 const nameInput = document.querySelector('.input-name');
 const authorInput = document.querySelector('.input-author');
 const pagesInput = document.querySelector('.input-pages');
-const imageInput = document.querySelector('.input-image');
 const colorInput = document.querySelector('.input-color');
 const mainContainer = document.querySelector('.main__container');
-const card = document.querySelector('.card');
+let card = document.querySelectorAll('.card');
 
 
 addButton.addEventListener('click', () => {
@@ -76,7 +91,8 @@ closeButton.addEventListener('click', () => {
     cardInputDisplay.classList.remove('active');
 });
 
-submitButton.addEventListener('click', () => {
-    addBookToLibrary()
-    cardInputDisplay.classList.remove('active');
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addBookToLibrary();
 });
