8dc1e0d0a9707295c76e949ceb2c254ee2d54cf9
Kai Takami <kaitakami@protonmail.com>
2022-07-26 19:50:09 +0900

    Create form validation with Constraint validation API

--- app.js
+++ app.js
@@ -1,81 +1,79 @@
-let myLibrary = [];
+const myLibrary = [];
 
 // Function constructor
 class Book {
-    constructor(title, author, pages, color) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.color = color;
-    }
+  constructor(title, author, pages, color) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.color = color;
+  }
 }
-    
+
 // The magic
 function addBookToLibrary() {
-    const title = nameInput.value;
-    const author = authorInput.value;
-    const pages = pagesInput.value;
-    const color = colorInput.value;
+  const title = nameInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const color = colorInput.value;
 
-    // Create new object
-    const book = new Book(title, author, pages, color)
+  // Create new object
+  const book = new Book(title, author, pages, color);
 
-    myLibrary.push(book);
+  myLibrary.push(book);
 
-    //Validate
-    if (title === '' || author === '' || pages === '') {
-        alert('Fill all the blanks');
-    } else {
-        // Display book DOM
-        const cardDisplay = mainContainer.appendChild(document.createElement('div'));
-        cardDisplay.classList.add('card');
-        const colorDisplay = cardDisplay.appendChild(document.createElement('span'));
-        colorDisplay.classList.add('card__color');
-        colorDisplay.style.backgroundColor = color;
-        const cardContent = cardDisplay.appendChild(document.createElement('div'));
-        cardContent.classList.add('card__content');
-        const titleDisplay = cardContent.appendChild(document.createElement('h3'));
-        titleDisplay.classList.add('book-title', 'card__text');
-        titleDisplay.textContent = `Title: ${title}`;
-        const authorDisplay = cardContent.appendChild(document.createElement('p'));
-        authorDisplay.classList.add('book-author', 'card__text');
-        authorDisplay.textContent = `Author: ${author}`;
-        const pagesDisplay = cardContent.appendChild(document.createElement('p'));
-        pagesDisplay.classList.add('book-pages', 'card__text');
-        pagesDisplay.textContent = `Pages: ${pages}`;
+  // Validate
+  if (title === '' || author === '' || pages === '') {
+    return;
+  } else {
+    // Display book DOM
+    const cardDisplay = mainContainer.appendChild(document.createElement('div'));
+    cardDisplay.classList.add('card');
+    const colorDisplay = cardDisplay.appendChild(document.createElement('span'));
+    colorDisplay.classList.add('card__color');
+    colorDisplay.style.backgroundColor = color;
+    const cardContent = cardDisplay.appendChild(document.createElement('div'));
+    cardContent.classList.add('card__content');
+    const titleDisplay = cardContent.appendChild(document.createElement('h3'));
+    titleDisplay.classList.add('book-title', 'card__text');
+    titleDisplay.textContent = `Title: ${title}`;
+    const authorDisplay = cardContent.appendChild(document.createElement('p'));
+    authorDisplay.classList.add('book-author', 'card__text');
+    authorDisplay.textContent = `Author: ${author}`;
+    const pagesDisplay = cardContent.appendChild(document.createElement('p'));
+    pagesDisplay.classList.add('book-pages', 'card__text');
+    pagesDisplay.textContent = `Pages: ${pages}`;
 
-        // Remove button DOM
-        const removeButtonDisplay = cardContent.appendChild(document.createElement('i'));
-        removeButtonDisplay.classList.add('fa-solid', 'fa-circle-minus', 'remove-button');
+    // Remove button DOM
+    const removeButtonDisplay = cardContent.appendChild(document.createElement('i'));
+    removeButtonDisplay.classList.add('fa-solid', 'fa-circle-minus', 'remove-button');
 
-        // Remove button
-        card = document.querySelectorAll('.card');
-        for (let i = 0; i < card.length; i++) {
-            card[i].addEventListener('click', (e) => {
-                if (e.target.classList.contains('remove-button')) {
-                    e.target.parentElement.parentElement.remove()
-                }
-            })
+    // Remove button
+    card = document.querySelectorAll('.card');
+    for (let i = 0; i < card.length; i++) {
+      card[i].addEventListener('click', (e) => {
+        if (e.target.classList.contains('remove-button')) {
+          e.target.parentElement.parentElement.remove();
         }
-
-        // Clear form
-        nameInput.value = '';
-        authorInput.value = '';
-        pagesInput.value = '';
-        colorInput.value = '#000000';
-
-        // Close form
-        cardInputDisplay.classList.remove('active');
+      });
     }
-}
 
+    // Clear form
+    nameInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    colorInput.value = '#000000';
+
+    // Close form
+    cardInputDisplay.classList.remove('active');
+  }
+}
 
 
 // DOM
 const addButton = document.querySelector('.add-button');
 const closeButton = document.querySelector('.close-button');
 const submitButton = document.querySelector('.submit-button');
-const removeButton = document.querySelectorAll('.remove-button');
 const cardInputDisplay = document.querySelector('.input-card');
 const nameInput = document.querySelector('.input-name');
 const authorInput = document.querySelector('.input-author');
@@ -86,15 +84,40 @@ let card = document.querySelectorAll('.card');
 
 
 addButton.addEventListener('click', () => {
-    cardInputDisplay.classList.toggle('active')
-})
+  cardInputDisplay.classList.toggle('active');
+});
 
 closeButton.addEventListener('click', () => {
-    cardInputDisplay.classList.remove('active');
+  cardInputDisplay.classList.remove('active');
 });
 
 submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
+  addBookToLibrary();
+});
 
-    addBookToLibrary();
+// Validate form
+nameInput.addEventListener('input', () => {
+  nameInput.setCustomValidity('');
+  nameInput.checkValidity();
+});
+
+nameInput.addEventListener('invalid', () => {
+  if (nameInput.value === '') {
+    nameInput.setCustomValidity('Write the book name');
+  } else {
+    nameInput.setCustomValidity('Book titles can only contain upper and lowercase letters. Try again!');
+  }
+});
+
+pagesInput.addEventListener('input', () => {
+  pagesInput.setCustomValidity('');
+  pagesInput.checkValidity();
+});
+
+pagesInput.addEventListener('invalid', () => {
+  if (nameInput.value === '') {
+    nameInput.setCustomValidity('Insert the pages number');
+  } else {
+    nameInput.setCustomValidity('Insert a number bigger than one');
+  }
 });
